a79a0e4aab79e37a400aa9008a394fcca95d46e1
Tadas Subonis <tadas.subonis@gmail.com>
2019-10-21 15:00:42 +0200

    first sample

---
+++ src/main/java/com/acme/app/Controller.java
@@ -0,0 +1,4 @@
+package com.acme.app;
+
+public class Controller {
+}
---
+++ src/main/java/com/acme/app/Main.java
@@ -0,0 +1,26 @@
+package com.acme.app;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        GridPane root = (GridPane) FXMLLoader.load(getClass().getResource("/sample.fxml"));
+        root.getChildren().addAll(new Label("Hello world"));
+
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
